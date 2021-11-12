769b61fb80ca44082766dbad6c8f03cd932dfb01
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-11-12 16:05:05 +0200

    [DOMD][HACK] initialize vpci right before startng the domain
    In vpci_domain_init we rely on used_by property for the PCI host bridges
    to be set in order to detect if this is a PCI hardware domain.
    The start sequence is that vpci_domain_init is called first and then
    the toolstack assigns devices with XEN_DOMCTL_assign_device and used_by
    is properly set. This leads to the wrong assumption on PCI hardware
    domain and results in the fact that MMIO handlers for the DomD are set
    as for other guest domain, e.g. with virtual ECAM host bridge.
    
    Work this around by assigning MMIO handlers late right before jumping to
    the guest.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/arch/arm/domain.c
+++ xen/arch/arm/domain.c
@@ -777,9 +777,6 @@ int arch_domain_create(struct domain *d,
     if ( is_hardware_domain(d) && (rc = domain_vuart_init(d)) )
         goto fail;
 
-    if ( (rc = domain_vpci_init(d)) != 0 )
-        goto fail;
-
 #ifdef CONFIG_ARM64_SVE
     /* Copy the encoded vector length sve_vl from the domain configuration */
     d->arch.sve_vl = config->arch.sve_vl;
@@ -870,6 +867,8 @@ int arch_domain_soft_reset(struct domain *d)
 
 void arch_domain_creation_finished(struct domain *d)
 {
+    ASSERT(!domain_vpci_init(d));
+
     /*
      * To avoid flushing the whole guest RAM on the first Set/Way, we
      * invalidate the P2M to track what has been accessed.
